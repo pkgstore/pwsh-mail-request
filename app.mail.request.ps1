e435e85c38af0436f545a3968abd8f1b7c8c1d6a
KaiKimera <git@kai.kim>
2025-12-16 13:52:52 +0300

    Update resources

--- app.mail.request.ps1
+++ app.mail.request.ps1
@@ -40,6 +40,8 @@ param(
   [ValidatePattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$')][string[]]$Bcc,
   [SupportsWildcards()][string[]]$Request,
   [ValidateSet('Low', 'Normal', 'High')][string]$Priority = 'Normal',
+  [string]$LogPath = "${PSScriptRoot}\log.mail.request.txt",
+  [string]$LogSize = '50MB',
   [switch]$Attach,
   [switch]$Save,
   [switch]$HTML,
@@ -48,9 +50,9 @@ param(
 )
 
 $Cfg = ((Get-Item "${PSCommandPath}").Basename + '.ini')
-$P = (Get-Content -Path "${PSScriptRoot}\${Cfg}" | ConvertFrom-StringData)
-$Log = "${PSScriptRoot}\log.mail.request.txt"
+$P = (Get-Content "${PSScriptRoot}\${Cfg}" | ConvertFrom-StringData)
 $Request = (Resolve-Path "${Request}" | Select-Object -ExpandProperty 'Path'); if ($null -eq $Request ) { exit }
+$TS = (Get-Date -UFormat '%F.%H-%M-%S')
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # -----------------------------------------------------< SCRIPT >----------------------------------------------------- #
@@ -60,24 +62,30 @@ function Remove-Request([string]$Path) {
   Remove-Item -LiteralPath "${Path}"
 }
 
+function Compress-Log([string]$Path, [string]$Size) {
+  if ((Get-Item $Path).Length -gt $Size) {
+    Compress-Archive "${Path}" -DestinationPath "${Path}.${TS}.zip" && Remove-Item "${Path}"
+  }
+}
+
 function Send-Mail {
   try {
     $Request.ForEach({
       $Mail = (New-Object System.Net.Mail.MailMessage)
-      $Mail.Subject = ((Get-Content "${_}" | Select-Object -Skip 1 -First 1) -replace '^(Subject: )','')
+      $Mail.Subject = ((Get-Content "${_}" | Select-String '^(Subject: (.*))').Matches.Groups[2].Value)
       $Mail.Body = (Get-Content "${_}" | Select-Object -Skip 2)
       $Mail.BodyEncoding= [System.Text.Encoding]::UTF8
       $Mail.From = $From
       $Mail.Priority = $Priority
       $Mail.IsBodyHtml = $HTML
-      $To = (((Get-Content "${_}" | Select-Object -First 1) -replace '^(To: )','') -split ',')
-      $To.ForEach({ $Mail.To.Add(-join($_,'@', $Domain)) })
+      $To = (((Get-Content "${_}" | Select-String '^(To: (.*))').Matches.Groups[2].Value) -split ',')
+      $To.ForEach({ $Mail.To.Add(-join($_, '@', $Domain)) })
       $Cc.ForEach({ $Mail.CC.Add($_) })
       $Bcc.ForEach({ $Mail.BCC.Add($_) })
       if ($Attach) { $Mail.Attachments.Add((New-Object System.Net.Mail.Attachment($_))) }
       if ($BypassCertValid) { [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true } }
 
-      $RequestName = (Split-Path -Path "${_}" -LeafBase)
+      $RequestName = (Split-Path "${_}" -LeafBase)
       $MailName = ($Mail.To | Join-String -Separator ', ')
 
       $SmtpClient = (New-Object Net.Mail.SmtpClient($P.Server, $P.Port))
@@ -94,7 +102,6 @@ function Send-Mail {
 }
 
 function Start-Script() {
-  Start-Transcript -Path "${Log}"
-  Send-Mail
-  Stop-Transcript
+  Start-Transcript "${LogPath}" -Append; Send-Mail; Stop-Transcript
+  Compress-Log "${LogPath}" -Size "${LogSize}"
 }; Start-Script
